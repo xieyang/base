ef8cb33e2a8528e4b74d3155f39c46da5f2569cb
xieyang <372282766@qq.com>
2015-12-17 00:09:41 +0800

    添加常量类

---
+++ pool/src/main/java/com/xieyang/jsch/JSCHContstants.java
@@ -0,0 +1,15 @@
+package com.xieyang.jsch;
+
+/**
+ * JSCH Constants
+ * 
+ * 
+ * @author 谢阳
+ * @since 1.0
+ * @version 2015年12月16日 谢阳
+ */
+public class JSCHContstants {
+
+	/** 默认的端口 */
+	public static final int DEFAULT_PORT = 22;
+}
